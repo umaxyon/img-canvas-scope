f8bdca182732df7b8514ec7ff0d20b0ed3ae8193
Umaxyon <umaxyon@gmail.com>
2021-02-15 13:14:06 +0900

    webpackにTerserPlugin追加。(minify)

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,34 @@
 const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+const { isDate } = require('util');
 
-module.exports = {
-    mode: 'development',
+const IS_DEVELOP = true;
+
+const conf = {
     entry: './src/img-canvas-scope.js',
     output: {
         filename: 'img-canvas-scope.js',
         path: path.join(__dirname, 'dist')
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    compress: true,
+                    compress: { drop_console: true }
+                }
+            })
+        ]
     }
 };
+
+if (IS_DEVELOP) {
+    conf.mode = 'development';
+    conf.devtool = 'source-map';
+} else {
+    conf.mode = 'production';
+}
+
+module.exports = conf;
+
